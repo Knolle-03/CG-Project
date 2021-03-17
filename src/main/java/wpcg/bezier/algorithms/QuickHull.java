444c53c211089f3cc223cfd22af499d4fcd367ca
Knolle-03 <willem-lennart.draeger@haw-hamburg.de>
2021-03-17 09:28:02 +0100

    convex hull algo works ... finally

--- src/main/java/wpcg/bezier/algorithms/QuickHull.java
+++ src/main/java/wpcg/bezier/algorithms/QuickHull.java
@@ -36,14 +36,14 @@ public class QuickHull {
         return convexHull;
     }
 
-    private static void findHull(List<Vector2f> subList, Vector2f v1, Vector2f v2, List<Vector2f> convexHull) {
+     static void findHull(List<Vector2f> subList, Vector2f v1, Vector2f v2, List<Vector2f> convexHull) {
         if (subList.isEmpty()) return;
 
         Vector2f farthest = subList.get(0);
-        float longestDistance = calcDistanceToLine(v1, v2, farthest);
+        double longestDistance = calcDistanceToLine(v1, v2, farthest);
         for (int i = 1; i < subList.size(); i++) {
             Vector2f current = subList.get(i);
-            float currentDistance = calcDistanceToLine(v1, v2, current);
+            double currentDistance = calcDistanceToLine(v1, v2, current);
             if (currentDistance > longestDistance) {
                 longestDistance = currentDistance;
                 farthest = current;
@@ -54,7 +54,8 @@ public class QuickHull {
         List<Vector2f> abovePC = new ArrayList<>();
         List<Vector2f> aboveCQ = new ArrayList<>();
         // sort point in above and below the line described by leftMost and rightMost
-        //subList.remove(farthest);
+
+        subList.remove(farthest);
 
 
         for (int i = 0; i < subList.size(); i++) {
@@ -73,26 +74,12 @@ public class QuickHull {
         findHull(aboveCQ, farthest, v2, convexHull);
     }
 
-    private static float calcDistanceToLine(Vector2f v1, Vector2f v2, Vector2f point) {
-
-        float l2 = v1.distanceSquared(v2);
-        if (l2 == 0) return point.distance(v1);
-
-        Vector2f v1_point = new Vector2f(point.subtract(v1));
-        Vector2f v1_v2 = new Vector2f(v1.subtract(v1));
-        float res = v1_point.dot(v1_v2) / l2;
-        float t = Math.max(0, Math.min(1, res));
-
-        Vector2f projection = (v2.subtract(v1)).mult(t).add(v1);
-
-        return point.distance(projection);
-
-//        float slope = l2.y - l1.y / l2.y - l1.x;
-//        float yIntersect = l1.y - slope * l1.x;
-//        return Math.abs(-slope * point.x + point.y - yIntersect) / (float) Math.sqrt((slope * slope) + 1);
+     static double calcDistanceToLine(Vector2f v1, Vector2f v2, Vector2f point) {
+        return (Math.abs((v2.x - v1.x) * (v1.y - point.y) - (v1.x - point.x) * (v2.y - v1.y)) /
+                Math.sqrt((Math.pow((v2.x - v1.x), 2) + Math.pow( v2.y - v1.y , 2)))  );
     }
 
-    private static void aboveAndBelow(List<Vector2f> points, Vector2f leftMost, Vector2f rightMost, List<Vector2f> above, List<Vector2f> below) {
+     static void aboveAndBelow(List<Vector2f> points, Vector2f leftMost, Vector2f rightMost, List<Vector2f> above, List<Vector2f> below) {
         System.out.println("Points size: " + points.size());
         int added = 0;
         //Vector2f v1 = new Vector2f(rightMost.x - leftMost.x, rightMost.y - leftMost.y);
